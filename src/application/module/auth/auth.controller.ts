b52595db177d8400f10f7b15561e26713256e635
jameelf22labs <jameel.m@f22labs.com>
2025-07-17 10:21:48 +0530

    error pass to next errror middleware

--- src/application/module/auth/auth.controller.ts
+++ src/application/module/auth/auth.controller.ts
@@ -2,12 +2,11 @@ import { NextFunction, Request, Response } from "express";
 import { IAuthService } from "./interface/service/auth.service.interface";
 import { SignUpDto, LoginDto } from "./interface/dto/request.dto";
 import { validateSignUpPayload, validateLoginPayload } from "./auth.validator";
-import ValidationError from "../../errors/ValidationError";
 
 export default class AuthController {
   constructor(private readonly service: IAuthService) {}
 
-  async httpSignUp(request: Request, response: Response) {
+  async httpSignUp(request: Request, response: Response, next: NextFunction) {
     try {
       const signupPayload: SignUpDto = request.body;
       const validatedPayload = await validateSignUpPayload(signupPayload);
@@ -17,21 +16,11 @@ export default class AuthController {
         message: "Successfully created an account",
       });
     } catch (error) {
-      console.error(error);
-
-      if (error instanceof ValidationError) {
-        return response.status(error.statusCode).json({
-          message: error.message,
-        });
-      }
-
-      return response.status(500).json({
-        message: "Internal Server Error",
-      });
+      next(error);
     }
   }
 
-  async httpLogin(request: Request, response: Response) {
+  async httpLogin(request: Request, response: Response, next: NextFunction) {
     try {
       const loginPayload: LoginDto = request.body;
       const validatedPayload = await validateLoginPayload(loginPayload);
@@ -41,16 +30,7 @@ export default class AuthController {
         message: "Successfully logged in",
       });
     } catch (error) {
-      if (error instanceof ValidationError) {
-        console.error(error.stack);
-        return response.status(error.statusCode).json({
-          message: error.message,
-        });
-      }
-
-      return response.status(500).json({
-        message: "Internal Server Error",
-      });
+      next(error);
     }
   }
 }
